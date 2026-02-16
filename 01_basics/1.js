2b6d066ffbfaf2a866309976cc78d79bf74c9010
DevenVasara <24bce035@nirmauni.ac.in>
2026-02-16 14:34:53 +0000

    1

---
+++ 01_basics/1.js
@@ -0,0 +1,12 @@
+const account_Id=12345
+let account_email="Deven@gmail.com"
+var account_Password=787878
+account_City="Ahemdabad"
+
+
+account_email="Me@gmail.com"
+account_Password=11232323
+account_City="Junagadh"
+
+
+console.table([account_Id,account_email,account_Password,account_City])
